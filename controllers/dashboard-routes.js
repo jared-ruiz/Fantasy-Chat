61fa2c52b8ebcaae67603e6d4c789ae56fded045
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-05-14 15:55:15 -0500

    removed comment const because we dont have it yet and remove withauth because bcrypt and auth not implimented

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment,} = require('../models/index.js');
-const withAuth = require('../utils/auth');
+const { Post, User,} = require('../models/index.js');
 
-router.get('/', withAuth, (req, res) => {
+router.get('/', (req, res) => {
     Post.findAll({
       where: {
         // use the ID from the session
