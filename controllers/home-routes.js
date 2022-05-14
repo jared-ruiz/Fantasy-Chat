31ab8a366c8e37ddec6d700b05ec124a27b61e03
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-05-14 15:59:30 -0500

    added dashboard routes to index.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,5 +3,51 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
   res.render('homepage');
 });
+router.get('/', (req, res) => {
+  console.log('======================');
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -5,9 +5,13 @@ const apiRoutes = require('./api');
 //home routes
 const homeRoutes = require('./home-routes.js');
 router.use('/', homeRoutes);
+//dashboard routes
+const dashboardRoutes = require('./dashboard-routes.js');
 
 //access api routes through /api ie: (/api/users/1)
 router.use('/api', apiRoutes);
+//dashboard use
+router.use('/dashboard', dashboardRoutes);
 
 //if requested route does not exist, trigger error
 router.use((req, res) => {
