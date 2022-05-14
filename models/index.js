cd5f6e341c462ace190f3d1fb2d8b177ea5ba8e7
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-05-14 15:41:32 -0500

    posts not created yet commented out unused code for later use

--- models/index.js
+++ models/index.js
@@ -1,10 +1,10 @@
 const User = require('./User');
 //model associations 
-User.hasMany(Post, {
-    foreignKey: 'user_id'
-})
-Post.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'Cascade'
-})
+// User.hasMany(Post, {
+//     foreignKey: 'user_id'
+// })
+// Post.belongsTo(User, {
+//     foreignKey: 'user_id',
+//     onDelete: 'Cascade'
+// })
 module.exports = { User };
