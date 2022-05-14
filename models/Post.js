5663cf0f14c65b2a847fe285e6ff9004bef3dc2e
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-05-14 15:36:16 -0500

    added post model and user and post associations

---
+++ models/Post.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+// create our Post model
+class Post extends Model {
+
+}
+//model associations 
+User.hasMany(Post, {
+    //id column in user conencts to user_id in post
+    foreignKey: 'user_id'
+})
+// create fields/columns for Post model
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    post_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: true
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+  }
+);
+
+
+module.exports = Post;
