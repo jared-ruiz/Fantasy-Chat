2c9126d9c6b3d9062cb09c9ee5f43d70c09cc3f8
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-05-14 15:53:45 -0500

    added dashboard routes in controllers

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment,} = require('../models/index.js');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+      where: {
+        // use the ID from the session
+        user_id: req.session.user_id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        // serialize data before passing to template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
