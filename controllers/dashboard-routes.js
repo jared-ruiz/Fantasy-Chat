5fce9be58946555dccaf7b864b0a5944e080a675
Jared Ruiz <jared707@gmail.com>
2022-05-16 18:56:37 -0500

    Added edit post route inside dashboard routes.js to enable rendering of the edit post handlebars.

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -36,4 +36,52 @@ router.get("/", withAuth, (req, res) => {
     });
 });
 
+//edit-post dashboard route
+router.get('/edit/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbPostData => {
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No post found with that id' });
+      return;
+    }
+    
+    //serialize data
+    const post = dbPostData.get({ plain: true });
+
+    //render edit-posts handlebars after success
+    res.render('edit-post', {
+      post,
+      loggedIn: true
+    })
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+})
+
 module.exports = router;
