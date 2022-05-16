7bc79c4fc25b47eae5f28401a513e9bc874321fa
MrCurls <JonCurls@gmail.com>
2022-05-15 22:50:10 -0500

    handlebars for post info and single post

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const sequelize = require('./config/connection');
-const express = require('express');
-const path = require('path');
-const routes = require('./controllers');
+const sequelize = require("./config/connection");
+const express = require("express");
+const path = require("path");
+const routes = require("./controllers");
 //allows us to use the dotevn contents via process.env
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,11 +11,11 @@ const PORT = process.env.PORT || 3001;
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //sessions + sequelize modules
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 //secret will be located in the .env file. Create a variable that matches the reference below
 const sess = {
   secret: process.env.session_secret,
@@ -23,19 +23,20 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
-}
+    db: sequelize,
+  }),
+};
 
 // use session
 app.use(session(sess));
 
 //handlebars template engine code
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const exphbs = require("express-handlebars");
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 //utilize routes
 app.use(routes);
@@ -44,4 +45,4 @@ app.use(routes);
 //switch to true when dropping tables (when updating table relationship logic)
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
-})
+});
