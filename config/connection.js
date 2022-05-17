d92f2b1b8ffabfe184d5119b89f6b2f0da832d32
Jared Ruiz <jared707@gmail.com>
2022-05-16 23:23:18 -0500

    Added JAWSDB functionality and first draft of readme.

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,18 @@ const Sequelize = require('sequelize');
 //execute upon server connection // uses .env credentials 
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-})
+let sequelize;
+
+//JAWSDB connection
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
+}
+else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    })
+}
 
 module.exports = sequelize;
